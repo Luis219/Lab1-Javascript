5b1e32c8068721293ad08f2d281c774252972852
pluis9 <luispalcivar@gmail.com>
2022-06-26 17:53:11 -0500

    primeros pasos del juego

--- app.py
+++ app.py
@@ -17,6 +17,21 @@ app.secret_key = "luisparedez"
 #rutas de la carpeta templates/static
 app._static_folder = os.path.abspath("templates/static/")
 
+#ruta de la página principal index
+@app.route("/")
+def index():
+    """
+    funcion que renderiza la página principal
+
+    Retorno
+    ------
+    Renderiza la página principal (block_hopper.html)
+
+    """
+
+    return render_template("layouts/block_hopper.html")
+
+
 if __name__ == "__main__":
     """
     Ejecucion de la aplicacion
--- templates/static/js/block_hopper.js
+++ templates/static/js/block_hopper.js
@@ -1,15 +1,23 @@
-
+//anchura de canvas
 var canvasWidth=580;
+//altura de canvas
 var canvasHeight=470;
 
 function startGame(){
-     gameCanvas.start();
+    /**
+   * Funcion que inicializa el juego
+   */
+    gameCanvas.start();
 }
 
 var gameCanvas={
+    //se crea el elemento canvas
     canvas: document.createElement("canvas"),
     start: function(){
-        this.canvas.width=canvasHeight;
+        /**
+         * Se establece la anchura y altura declarada anteriormente
+        */
+        this.canvas.width=canvasWidth;
         this.canvas.height= canvasHeight;
         this.context= this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
